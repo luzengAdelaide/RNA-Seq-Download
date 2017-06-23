09e1f17985b102fb4b5d46e4a5ae040b3ae45a43
sophielu <sophiezenglu@126.com>
2017-06-23 11:45:03 +0930

    add file

---
+++ Download_sepcies_gff_fa.sh
@@ -0,0 +1,22 @@
+# Download gtf data and fasta sequence data
+
+# Opossum
+wget ftp://ftp.ensembl.org/pub/release-83/fasta/monodelphis_domestica/dna/Monodelphis_domestica.BROADO5.dna.toplevel.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-83/gtf/monodelphis_domestica/Monodelphis_domestica.BROADO5.83.gtf.gz
+
+# Platypus
+wget ftp://ftp.ensembl.org/pub/release-83/fasta/ornithorhynchus_anatinus/dna/Ornithorhynchus_anatinus.OANA5.dna.toplevel.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-83/gtf/ornithorhynchus_anatinus/ornithorhynchus_anatinus.BROADO5.83.gtf.gz
+
+# Chicken
+wget ftp://ftp.ensembl.org/pub/release-83/fasta/gallus_gallus/dna/Gallus_gallus.Galgal4.dna.toplevel.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-83/gtf/gallus_gallus/Gallus_gallus.Galgal4.83.gtf.gz
+
+# Anolis
+wget ftp://ftp.ensembl.org/pub/release-83/fasta/anolis_carolinensis/dna/Anolis_carolinensis.AnoCar2.0.dna.toplevel.fa.gz
+wget ftp://ftp.ensembl.org/pub/release-83/gtf/anolis_carolinensis/Anolis_carolinensis.AnoCar2.0.83.gtf.gz
+
+# Human fasta sequence and gtf was downloaded from UCSC in 2015
+
+# Bearded dragon and echidna were from private source, while you can download bearded dragon genome and gff files from NCBI
+https://www.ncbi.nlm.nih.gov/genome/7589?genome_assembly_id=313528
