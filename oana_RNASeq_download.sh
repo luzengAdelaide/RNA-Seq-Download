5b33808af7f05273a5a030faee8a1fda828fa60b
sophielu <sophiezenglu@126.com>
2017-06-25 18:01:52 +0930

    add ovary data

--- oana_RNASeq_download.sh
+++ oana_RNASeq_download.sh
@@ -11,5 +11,7 @@ do
 done
 
 # Download data from Uni of Arizona
-
+# Ovary
 wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByExp/sra/SRX/SRX217/SRX217699/SRR649382/SRR649382.sra
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR943/SRR943349/SRR943349.sra
+wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/SRR943/SRR943350/SRR943350.sra
